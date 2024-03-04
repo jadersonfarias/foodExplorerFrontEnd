6020738fc47d64a75ea31751deedf13f32d01ef4
jaderson <jader375@gmail.com>
2024-03-04 17:29:31 -0300

    update

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -39,7 +39,7 @@ export const LogoAuth = styled.div`
 `
 
 export const Form = styled.form`  
-    padding: 6.4rem; 
+   // padding: 6.4rem; 
  
 
     background-color: ${({theme}) => theme.COLORS.DARK['700']};
@@ -53,10 +53,10 @@ export const Form = styled.form`
 
 
     @media (max-width:${DEVICE_BREAKPOINTS.SM}) {
-           height: 100vh;
+           //height: 100vh;
            background-color:transparent;
            align-items: center;
-           padding: 0;
+           
        ;          
     }
 
